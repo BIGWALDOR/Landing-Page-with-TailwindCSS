7ff320ab17da86d36cba5bf903aac94998445438
Walid Bouladam <bouladam.walid86@gmail.com>
2021-11-29 21:30:13 +1300

    Added colors that will be used for this project to the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
     container: {
       center: true,
       padding: '1rem',
